f0c74a0e8d8203c2930f19041b663d4b638ab5d5
Hipy Cas <hipycas@gmail.com>
2023-11-13 05:46:33 +0100

    Notifications

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
-<template><RouterView /></template>
+<template>
+  <RouterView />
+  <notifications position="bottom center">
+    <template #body="props">
+      <div class="alert flex flex-row mb-4" :class="`alert-${props.item.type}`">
+        <component
+          class="flex-grow-0 flex-shrink-0"
+          :is="notificationComponent(props.item.type)"
+        ></component>
+        <p class="flex-grow text-left">{{ props.item.text }}</p>
+        <button class="flex-grow-0 flex-shrink-0" @click="props.close">
+          <i class="fa fa-fw fa-close"></i>
+        </button>
+      </div>
+    </template>
+  </notifications>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IconAlertTriangle, IconCircleCheck, IconCircleX, IconInfoCircle } from '@tabler/icons-vue'
+
+const notificationComponent = (type: string) =>
+  ({
+    error: IconCircleX,
+    warning: IconAlertTriangle,
+    success: IconCircleCheck,
+    info: IconInfoCircle
+  })[type]
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Notifications from '@kyvg/vue3-notification'
 
 import './index.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router).use(Notifications)
 
 app.mount('#app')
